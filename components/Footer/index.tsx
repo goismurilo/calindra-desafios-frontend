115ff7a2b1db9f9b9cad1d698242007e95a2b19c
Murilo Gois <murilogois59@gmail.com>
2022-04-02 20:20:12 -0300

    feat(Footer): Cria footer

---
+++ components/Footer/index.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+
+import facebookIcon from '../../src/assets/icons/facebook_icon.png'
+import twitterIcon from '../../src/assets/icons/twitter_icon.png'
+import youtubeIcon from '../../src/assets/icons/youtube_icon.png'
+
+export default function Footer() {
+    return (
+        <footer className='md:flex md:justify-center'>
+            {/* Redes Sociais */}
+            <div className='max-w-7xl mx-auto px-4 pt-16 pb-2 md:text-left lg:px-8 items-center text-center'>
+                <span className='font-bold text-3xl text-primary-text'>Follow Us</span>
+                <div className='w-4xl px-4 md:px-0 py-6 items-center flex justify-center'>
+                    <div className='px-3'>
+                        <Image src={youtubeIcon} alt="Icone do Youtube" />
+                    </div>
+                    <div className='px-3'>
+                        <Image src={facebookIcon} alt="Icone do Facebook" />
+                    </div>
+                    <div className='px-3'>
+                        <Image src={twitterIcon} alt="Icone do Twitter" />
+                    </div>
+                </div>
+            </div>
+            {/* Contatos */}
+            <div className='max-w-7xl md:w-6/12 mx-auto md:m-0  px-4 md:pt-16 text-center md:text-left lg:px-8 items-center'>
+                <span className='font-bold text-3xl text-primary-text'>Contact</span>
+                <div className='py-6'>
+                    <p className='font-normal text-primary-text text-lg'>2490 Leisure Lane<br />
+                        San Luis Obispo<br />
+                        California</p>
+                </div>
+            </div>
+        </footer>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Footer from '../components/Footer';
 import Navbar from '../components/Navbar'
 
 import rolledTape from '../src/assets/images/rolled-tape.png';
@@ -45,18 +46,7 @@ const Home: NextPage = () => {
 				</div>
 			</main >
 
-			<footer >
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Powered by{' '}
-					<span >
-						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-					</span>
-				</a>
-			</footer>
+			<Footer />
 		</div >
 	)
 }
