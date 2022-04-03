b38d437240cc2cdee76eb43a20ff9fb94ef0df01
Murilo Gois <murilogois59@gmail.com>
2022-04-03 08:03:38 -0300

    fix(Desafio02): Conserta cor da fonte no footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
         <footer className='md:flex md:justify-center'>
             {/* Redes Sociais */}
             <div className='max-w-7xl mx-auto px-4 pt-16 pb-2 md:text-left lg:px-8 items-center text-center'>
-                <span className='font-bold text-3xl text-primary-text'>Follow Us</span>
+                <span className='font-bold text-3xl text-secondary-text'>Follow Us</span>
                 <div className='w-4xl px-4 md:px-0 py-6 items-center flex justify-center'>
                     <div className='px-3'>
                         <Image src={youtubeIcon} alt="Icone do Youtube" />
@@ -24,9 +24,9 @@ export default function Footer() {
             </div>
             {/* Contatos */}
             <div className='max-w-7xl md:w-6/12 mx-auto md:m-0  px-4 md:pt-16 text-center md:text-left lg:px-8 items-center'>
-                <span className='font-bold text-3xl text-primary-text'>Contact</span>
+                <span className='font-bold text-3xl text-secondary-text'>Contact</span>
                 <div className='py-6'>
-                    <p className='font-normal text-primary-text text-lg'>2490 Leisure Lane<br />
+                    <p className='font-normal text-secondary-text text-lg'>2490 Leisure Lane<br />
                         San Luis Obispo<br />
                         California</p>
                 </div>
